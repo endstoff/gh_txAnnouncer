1cc3df09e1b63abf332bcc55d4b3f9463179b49e
Jaccosf <69802108+Jaccosf@users.noreply.github.com>
2021-09-28 22:13:40 +0200

    tweak(client): little cleanup

--- cl/client.lua
+++ cl/client.lua
@@ -1,9 +1,7 @@
-RegisterNetEvent("gh_txAnnouncer:SendRestartMessage")
-AddEventHandler("gh_txAnnouncer:SendRestartMessage", function(timeLeft)
+RegisterNetEvent("gh_txAnnouncer:SendRestartMessage", function(timeLeft)
     SendRestartMessage(timeLeft)
 end)
 
-RegisterNetEvent("gh_txAnnouncer:SendKickedMessage")
-AddEventHandler("gh_txAnnouncer:SendKickedMessage", function(msg)
+RegisterNetEvent("gh_txAnnouncer:SendKickedMessage", function(msg)
     SendKickedMessage(msg)
 end)
