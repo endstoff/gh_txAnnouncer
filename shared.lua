4a26e84f998420c1680ab5b07a3c4982276ef546
endstoff <63933131+endstoff@users.noreply.github.com>
2024-03-18 18:23:15 +0100

    Add files via upload
    Readme adjusted, Announce added

--- shared.lua
+++ shared.lua
@@ -1,8 +1,11 @@
+---@diagnostic disable: undefined-global
+
 Shared = {}
 
-Shared.kickedNotify = true
-Shared.bannedNotify = true
-Shared.warnedNotify = true
+Shared.kickedNotify     = true
+Shared.bannedNotify     = true
+Shared.warnedNotify     = true
+Shared.announceNotify   = true
 
 -- Notifications
 function SendRestartMessage(timeLeft)
@@ -45,4 +48,24 @@ function SendKickedMessage(msg)
     }) 
     
     You can also use TriggerEvent() ]]  
+end
+
+function SendAnnounceMessage(msg)
+        -- You can comment this out. only for testing purpose
+        SetNotificationTextEntry('STRING')
+        AddTextComponentString(msg)
+        DrawNotification(0,1)
+        --
+    
+        --[[    Example Notification
+        exports['t-notify']:Custom({
+            style = 'info',
+            title = 'Information',
+            message = msg,
+            duration = 30000,
+            sound = true,
+            position = 'top-center',
+        }) 
+        
+        You can also use TriggerEvent() ]]  
 end
